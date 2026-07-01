a91dfa523ad0ae2ac115a8538e63634f78f22aad
ecadiz123 <ecadiz@ing.ucsc.cl>
2026-07-01 01:27:37 +0000

    cliente super nombre cambiado

--- supermercado.hpp
+++ supermercado.hpp
@@ -14,32 +14,35 @@ int out=0 ;
 std::counting_semaphore<B_size> vacios(B_size); //vacio inicializado con tamaño max
 std::counting_semaphore<B_size> lleno(0);
 std::mutex mut;
-void productor() {
+void cliente() {
     for (int i = 0; i < N_item; i++) {
         int item = i;               // dato a producir
 
         vacios.acquire();          // espera espacio libre
         mut.lock(); // entra a sección crítica
-
+		   
+	//Seccion critica
         buffer[in] = item;
         in = (in + 1) % B_size;
 
-        mut.unlock();
-        lleno.release();          // incrementa ítems disponibles
+        mut.unlock();//sale seccion critica
+        lleno.release();// incrementa ítems disponibles
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));//para salida
     }
 }
 
-void consumidor() {
+void cajero() {
     for (int i = 0; i < N_item; i++) {
+
         lleno.acquire();          // espera ítems disponibles
         mut.lock(); // entra a sección crítica
-
+		   
+	//Seccion critica
         int item = buffer[out];
         out = (out + 1) % B_size;
 
-        mut.unlock();
+        mut.unlock();//sale seccion critica
         vacios.release();          // incrementa espacios libres
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(2000));//para salida
